5fc53231f442b0c764f2e42a6cde57180a6a6fa0
Lacho18 <121165891+Lacho18@users.noreply.github.com>
2023-09-05 21:54:22 +0300

    Add files via upload

---
+++ SlideShow/slideContext.js
@@ -0,0 +1,64 @@
+const Slides = [
+    {
+        id: 1,
+        image: "Images/Tokio.jpg",
+        header: "Tokio",
+        context: "Tokio is the capital of Japan!",
+        headerColor: "black",
+        contextColor: "#ccffcc"
+    },
+
+    {
+        id: 2,
+        image: "Images/Tokio.jpg",
+        header: "Paris",
+        context: "Tokio is the capital of Japan!",
+        headerColor: "",
+        contextColor: ""
+    },
+
+    {
+        id: 3,
+        image: "Images/Tokio.jpg",
+        header: "Sofia",
+        context: "Tokio is the capital of Japan!",
+        headerColor: "",
+        contextColor: ""
+    },
+
+    {
+        id: 4,
+        image: "Images/Tokio.jpg",
+        header: "London",
+        context: "Tokio is the capital of Japan!",
+        headerColor: "",
+        contextColor: ""
+    },
+
+    {
+        id: 5,
+        image: "Images/Tokio.jpg",
+        header: "Rome",
+        context: "Tokio is the capital of Japan!",
+        headerColor: "",
+        contextColor: ""
+    },
+
+    {
+        id: 6,
+        image: "Images/Tokio.jpg",
+        header: "Madrid",
+        context: "Tokio is the capital of Japan!",
+        headerColor: "",
+        contextColor: ""
+    },
+
+    {
+        id: 7,
+        image: "Images/Tokio.jpg",
+        header: "Praga",
+        context: "Tokio is the capital of Japan!",
+        headerColor: "",
+        contextColor: ""
+    }
+];
